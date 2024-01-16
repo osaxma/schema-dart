9bc8e761a166550d5246072123115b0a1c663ec2
osaxma <46427323+osaxma@users.noreply.github.com>
2024-01-16 08:27:03 +0300

    add serialization/deserialization tests

---
+++ test/data/generate_data.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:postgres/postgres.dart';
+import 'package:schema_dart/src/schema_converter.dart';
+import 'package:test/test.dart';
+import 'package:path/path.dart' as p;
+
+import '../docker.dart';
+
+final serializationTestTableName = 'serialization_output';
+// final _outputFile = '$_tableName.g.dart';
+
+void main() {
+  withPostgresServer('Serialize/Deserialize tests', (server) {
+    late final Connection connection;
+    // late final String outputFilePath;
+    setUpAll(() async {
+      connection = await server.newConnection(
+        sslMode: SslMode.disable,
+      );
+      await generateTableAndRowsForSerializationTest(connection);
+      final parent = Directory.current.path;
+      final targetPath = p.join(parent, 'test/data');
+      // sanity check so we don't create files in unknown location
+      expect(p.isWithin(parent, '$targetPath/schema-dart/test/data'), true);
+      await SchemaConverter(
+        connectionString: await server.connectionString,
+        outputDirectory: Directory(targetPath),
+        schemaName: 'public',
+      ).convert();
+
+      // outputFilePath = p.join(targetPath, _outputFile);
+    });
+
+
+
+
+    test('deserialize', () {
+
+      
+
+
+    });
+
+
+
+  });
+
+
+}
+
+// shamelessly copied from `postgres-dart` decoding tests
+// https://github.com/isoos/postgresql-dart/blob/27d0064a992946060b9a19346df85a118deea774/test/decode_test.dart
+Future<void> generateTableAndRowsForSerializationTest(Connection connection) async {
+  // TODO: re-add `p point`
+  await connection.execute('''
+        CREATE TABLE public.$serializationTestTableName (
+          i int, s serial, bi bigint, bs bigserial, bl boolean, si smallint, 
+          t text, f real, d double precision, dt date, ts timestamp, tsz timestamptz, n numeric, j jsonb, ba bytea,
+          u uuid, v varchar, jj json, ia _int4, bia _int8, ta _text, da _float8, ja _jsonb, va varchar(20)[],
+          boola _bool
+        )
+    ''');
+
+  // TODO: re-add point
+  await connection
+      .execute('INSERT INTO public.$serializationTestTableName (i, bi, bl, si, t, f, d, dt, ts, tsz, n, j, ba, u, v, ' /*  p, */
+          'jj, ia, bia, ta, da, ja, va, boola) '
+          'VALUES (-2147483648, -9223372036854775808, TRUE, -32768, '
+          "'string', 10.0, 10.0, '1983-11-06', "
+          "'1983-11-06 06:00:00.000000', '1983-11-06 06:00:00.000000', "
+          "'-1234567890.0987654321', "
+          "'{\"key\":\"value\"}', E'\\\\000', '00000000-0000-0000-0000-000000000000', "
+          "'abcdef', " /* '(0.01, 12.34)', */
+          "'{\"key\": \"value\"}', '{}', '{}', '{}', '{}', '{}', "
+          "'{\"a\", \"b\", \"c\", \"d\", \"e\", \"f\"}', "
+          "'{true, false, false}'"
+          ')');
+
+  // TODO: re-add point
+  await connection
+      .execute('INSERT INTO public.$serializationTestTableName (i, bi, bl, si, t, f, d, dt, ts, tsz, n, j, ba, u, v, ' /*  p, */
+          'jj, ia, bia, ta, da, ja, va, boola) '
+          'VALUES (2147483647, 9223372036854775807, FALSE, 32767, '
+          "'a significantly longer string to the point where i doubt this actually matters', "
+          "10.25, 10.125, '2183-11-06', '2183-11-06 00:00:00.111111', "
+          "'2183-11-06 00:00:00.999999', "
+          "'1000000000000000000000000000.0000000000000000000000000001', "
+          "'[{\"key\":1}]', E'\\\\377', 'FFFFFFFF-ffff-ffff-ffff-ffffffffffff', "
+          "'01234', " /* '(0.2, 100)', */
+          "'{}', '{-123, 999}', '{-123, 999}', '{\"a\", \"lorem ipsum\", \"\"}', "
+          "'{1, 2, 4.5, 1234.5}', '{1, \"\\\"test\\\"\", \"{\\\"a\\\": \\\"b\\\"}\"}', "
+          "'{\"a\", \"b\", \"c\", \"d\", \"e\", \"f\"}', "
+          "'{false, false, true}' "
+          ')');
+
+  // TODO: re-add point
+  await connection
+      .execute('INSERT INTO public.$serializationTestTableName (i, bi, bl, si, t, f, d, dt, ts, tsz, n, j, ba, u, v, ' /* p, */
+          'jj, ia, bia, ta, da, ja, va, boola) '
+          'VALUES (null, null, null, null, null, null, null, null, null, null, null, null, null, null, '
+          'null, ' /* null, */
+          'null, null, null, null, null, null, null, null)');
+}
---
+++ test/data/serialization_output.g.dart
@@ -0,0 +1,269 @@
+import 'package:collection/collection.dart';
+import 'dart:convert';
+
+class SerializationOutput {
+  const SerializationOutput({
+    this.bl,
+    this.ta,
+    this.n,
+    this.ba,
+    this.va,
+    this.ia,
+    this.boola,
+    this.bia,
+    required this.s,
+    this.jj,
+    this.dt,
+    this.tsz,
+    this.v,
+    this.ts,
+    this.i,
+    this.da,
+    required this.bs,
+    this.ja,
+    this.u,
+    this.t,
+    this.d,
+    this.j,
+    this.bi,
+    this.f,
+    this.si,
+  });
+
+  factory SerializationOutput.fromMap(Map<String, dynamic> map) {
+    return SerializationOutput(
+      bl: map['bl'],
+      ta: map['ta'] == null ? null : List<String>.from(map['ta']),
+      n: map['n'],
+      ba: map['ba'] == null ? null : List<int>.from(map['ba']),
+      va: map['va'] == null ? null : List<String>.from(map['va']),
+      ia: map['ia'] == null ? null : List<int>.from(map['ia']),
+      boola: map['boola'] == null ? null : List<bool>.from(map['boola']),
+      bia: map['bia'] == null ? null : List<int>.from(map['bia']),
+      s: map['s'].toInt(),
+      jj: map['jj'],
+      dt: DateTime.tryParse(map['dt'] ?? ""),
+      tsz: DateTime.tryParse(map['tsz'] ?? ""),
+      v: map['v'],
+      ts: DateTime.tryParse(map['ts'] ?? ""),
+      i: map['i']?.toInt(),
+      da: map['da'] == null ? null : List<double>.from(map['da']),
+      bs: map['bs'].toInt(),
+      ja: map['ja'] == null ? null : List<Object>.from(map['ja']),
+      u: map['u'],
+      t: map['t'],
+      d: map['d']?.toDouble(),
+      j: map['j'],
+      bi: map['bi']?.toInt(),
+      f: map['f']?.toDouble(),
+      si: map['si']?.toInt(),
+    );
+  }
+
+  factory SerializationOutput.fromJson(String source) => SerializationOutput.fromMap(json.decode(source));
+
+  final bool? bl;
+
+  final List<String>? ta;
+
+  final String? n;
+
+  final List<int>? ba;
+
+  final List<String>? va;
+
+  final List<int>? ia;
+
+  final List<bool>? boola;
+
+  final List<int>? bia;
+
+  final int s;
+
+  final Object? jj;
+
+  final DateTime? dt;
+
+  final DateTime? tsz;
+
+  final String? v;
+
+  final DateTime? ts;
+
+  final int? i;
+
+  final List<double>? da;
+
+  final int bs;
+
+  final List<Object>? ja;
+
+  final String? u;
+
+  final String? t;
+
+  final double? d;
+
+  final Object? j;
+
+  final int? bi;
+
+  final double? f;
+
+  final int? si;
+
+  SerializationOutput copyWith({
+    bool? bl,
+    List<String>? ta,
+    String? n,
+    List<int>? ba,
+    List<String>? va,
+    List<int>? ia,
+    List<bool>? boola,
+    List<int>? bia,
+    int? s,
+    Object? jj,
+    DateTime? dt,
+    DateTime? tsz,
+    String? v,
+    DateTime? ts,
+    int? i,
+    List<double>? da,
+    int? bs,
+    List<Object>? ja,
+    String? u,
+    String? t,
+    double? d,
+    Object? j,
+    int? bi,
+    double? f,
+    int? si,
+  }) {
+    return SerializationOutput(
+      bl: bl ?? this.bl,
+      ta: ta ?? this.ta,
+      n: n ?? this.n,
+      ba: ba ?? this.ba,
+      va: va ?? this.va,
+      ia: ia ?? this.ia,
+      boola: boola ?? this.boola,
+      bia: bia ?? this.bia,
+      s: s ?? this.s,
+      jj: jj ?? this.jj,
+      dt: dt ?? this.dt,
+      tsz: tsz ?? this.tsz,
+      v: v ?? this.v,
+      ts: ts ?? this.ts,
+      i: i ?? this.i,
+      da: da ?? this.da,
+      bs: bs ?? this.bs,
+      ja: ja ?? this.ja,
+      u: u ?? this.u,
+      t: t ?? this.t,
+      d: d ?? this.d,
+      j: j ?? this.j,
+      bi: bi ?? this.bi,
+      f: f ?? this.f,
+      si: si ?? this.si,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'bl': bl,
+      'ta': ta,
+      'n': n,
+      'ba': ba,
+      'va': va,
+      'ia': ia,
+      'boola': boola,
+      'bia': bia,
+      's': s,
+      'jj': jj,
+      'dt': dt?.toUtc().toIso8601String(),
+      'tsz': tsz?.toUtc().toIso8601String(),
+      'v': v,
+      'ts': ts?.toUtc().toIso8601String(),
+      'i': i,
+      'da': da,
+      'bs': bs,
+      'ja': ja,
+      'u': u,
+      't': t,
+      'd': d,
+      'j': j,
+      'bi': bi,
+      'f': f,
+      'si': si,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    final collectionEquals = const DeepCollectionEquality().equals;
+
+    return other is SerializationOutput &&
+        other.bl == bl &&
+        collectionEquals(other.ta, ta) &&
+        other.n == n &&
+        collectionEquals(other.ba, ba) &&
+        collectionEquals(other.va, va) &&
+        collectionEquals(other.ia, ia) &&
+        collectionEquals(other.boola, boola) &&
+        collectionEquals(other.bia, bia) &&
+        other.s == s &&
+        other.jj == jj &&
+        other.dt == dt &&
+        other.tsz == tsz &&
+        other.v == v &&
+        other.ts == ts &&
+        other.i == i &&
+        collectionEquals(other.da, da) &&
+        other.bs == bs &&
+        collectionEquals(other.ja, ja) &&
+        other.u == u &&
+        other.t == t &&
+        other.d == d &&
+        other.j == j &&
+        other.bi == bi &&
+        other.f == f &&
+        other.si == si;
+  }
+
+  @override
+  int get hashCode {
+    return bl.hashCode ^
+        ta.hashCode ^
+        n.hashCode ^
+        ba.hashCode ^
+        va.hashCode ^
+        ia.hashCode ^
+        boola.hashCode ^
+        bia.hashCode ^
+        s.hashCode ^
+        jj.hashCode ^
+        dt.hashCode ^
+        tsz.hashCode ^
+        v.hashCode ^
+        ts.hashCode ^
+        i.hashCode ^
+        da.hashCode ^
+        bs.hashCode ^
+        ja.hashCode ^
+        u.hashCode ^
+        t.hashCode ^
+        d.hashCode ^
+        j.hashCode ^
+        bi.hashCode ^
+        f.hashCode ^
+        si.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'SerializationOutput(bl: $bl, ta: $ta, n: $n, ba: $ba, va: $va, ia: $ia, boola: $boola, bia: $bia, s: $s, jj: $jj, dt: $dt, tsz: $tsz, v: $v, ts: $ts, i: $i, da: $da, bs: $bs, ja: $ja, u: $u, t: $t, d: $d, j: $j, bi: $bi, f: $f, si: $si)';
+  }
+}
